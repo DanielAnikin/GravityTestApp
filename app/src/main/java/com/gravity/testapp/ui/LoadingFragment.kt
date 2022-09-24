9525a3615c06d1a1ff536dc0e3e4af0443b74211
DanielAnikin <daniknkn@gmail.com>
2022-09-24 22:36:01 +0300

    Edited LoadingFragment UI

--- app/src/main/java/com/gravity/testapp/ui/LoadingFragment.kt
+++ app/src/main/java/com/gravity/testapp/ui/LoadingFragment.kt
@@ -16,11 +16,6 @@ import kotlinx.coroutines.delay
 class LoadingFragment : Fragment(R.layout.fragment_loading) {
     private val viewModel: LinksViewModel by activityViewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        retainInstance = false
-    }
-
     private lateinit var binding: FragmentLoadingBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -30,7 +25,7 @@ class LoadingFragment : Fragment(R.layout.fragment_loading) {
         viewModel.status.observe(viewLifecycleOwner) { status ->
             lifecycleScope.launchWhenResumed {
                 if (status == DONE) {
-                    delay(1000) //Long loading imitation
+                    delay(2000) //Long loading imitation
                     findNavController().navigate(LoadingFragmentDirections.actionLoadingFragmentToWebViewFragment())
                 }
             }
